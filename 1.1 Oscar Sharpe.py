bc4b9e905267450b86f86fdbcb983617dc597027
osc808 <oscars.23111@obhs.school.nz>
2025-08-26 14:21:45 +1200

    Removed input asking for days worked out, and replaced it with a list of the days of the week
    The list will be used for the while loop

--- 1.1 Oscar Sharpe.py
+++ 1.1 Oscar Sharpe.py
@@ -14,8 +14,8 @@ def main():
     pushups_per_min = [ ]                           #Empty list that will have inputs added for the amount of pushups they were able to do
     name = input('Please enter your name: ')        #String input asking for the users name
     age = int(input('Please enter your age: '))     #Int input asking for the users age
-    days_worked_out = int(input('How many days would you like to track your progress over?: ')) #int input asking for how many days they want to work out for
-    for i in days_worked_out:
+    day_of_week = ['Monday','Tuesday','Wednesday','Thursday','Friday']  #List for the days of the week
+    for i in day_of_week:
         pushups_per_min.append(int(input('How many pushups were you able to do: ')))
 
 #---------main routine------------
