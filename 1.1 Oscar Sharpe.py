36ad1e5506054fd889787bf0a4d879720fe9ab10
osc808 <oscars.23111@obhs.school.nz>
2025-08-26 14:37:33 +1200

    Comments added to name validator

--- 1.1 Oscar Sharpe.py
+++ 1.1 Oscar Sharpe.py
@@ -12,15 +12,16 @@ import random
 #----------functions--------------
 def main():
     pushups_per_min = [ ]    #Empty list that will have inputs added for the amount of pushups they were able to do
-    while (True):
-        try:
+    while (True):               #Loop will loop until broken, which happens when the name is validated
+        try:                    #Try/except 
             name = input('Please enter your name: ')        #String input asking for the users name
-            if(len(name)>= 2 and len(name)<=20 and name.isalpha):
-                break
-            else:
+            if(len(name)>= 2 and len(name)<=20 and name.isalpha):   #Checks that the name length is more or equal to 2, and no longer than 20. It also checks that the input is a word and not a number
+                break                                #The loop will break and the code will carry on if the name is validated               
+            else:                                    #Else for if the input was not valid
                 print('That was not a valid input')
         except:
             print('That was not a valid input')
+
 
     age = int(input('Please enter your age: '))     #Int input asking for the users age
     day_of_week = ['Monday','Tuesday','Wednesday','Thursday','Friday']  #List for the days of the week
