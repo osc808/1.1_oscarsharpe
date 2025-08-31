556ec0052f1f0b80b893f9c0554241e546e13074
osc808 <oscars.23111@obhs.school.nz>
2025-09-01 11:48:06 +1200

    Name validator updated to only accept alpha input

--- 1.1 Oscar Sharpe.py
+++ 1.1 Oscar Sharpe.py
@@ -16,7 +16,7 @@ def main():
     while (True):               #Loop will loop until broken, which happens when the name is validated
         try:                    #Try/except 
             name = str(input('Please enter your name: '))        #String input asking for the users name
-            if(len(name)>= 2 and len(name)<=20 and name.isalpha):   #Checks that the name length is more or equal to 2, and no longer than 20. It also checks that the input is a word and not a number
+            if(len(name)>= 2 and len(name)<=15 and name.isalpha()):   #Checks that the name length is more or equal to 2, and no longer than 20. It also checks that the input is a word and not a number
                 break                                #The loop will break and the code will carry on if the name is validated               
             else:                                    #Else for if the input was not valid
                 print('That was not a valid input - Please enter text between 2-20 letters')    #Error message if input is not within range
